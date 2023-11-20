27af02ced818544e65f3d2862608ef1f995631df
feichashao <siwu@redhat.com>
2023-11-20 18:28:04 +0800

    add test for cluster handler

--- go.mod
+++ go.mod
@@ -37,16 +37,13 @@ require (
 	github.com/evanphx/json-patch v4.12.0+incompatible // indirect
 	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
 	github.com/exponent-io/jsonpath v0.0.0-20151013193312-d6023ce2651d // indirect
-	github.com/fatih/color v1.13.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-errors/errors v1.4.2 // indirect
 	github.com/go-logr/logr v1.2.4 // indirect
-	github.com/go-logr/zapr v1.2.4 // indirect
 	github.com/go-openapi/jsonpointer v0.19.6 // indirect
 	github.com/go-openapi/jsonreference v0.20.1 // indirect
 	github.com/go-openapi/swag v0.22.3 // indirect
 	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
-	github.com/gobuffalo/flect v0.3.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/golang/glog v1.0.0 // indirect
@@ -65,6 +62,16 @@ require (
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/itchyny/gojq v0.12.9 // indirect
+	github.com/itchyny/timefmt-go v0.1.4 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.13.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.1 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.12.0 // indirect
+	github.com/jackc/pgx/v4 v4.17.2 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
@@ -85,14 +92,9 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/monochromegane/go-gitignore v0.0.0-20200626010858-205db1a8cc00 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
-	github.com/nxadm/tail v1.4.8 // indirect
-	github.com/onsi/ginkgo v1.16.5 // indirect
-	github.com/openshift/api v3.9.1-0.20190424152011-77b8897ec79a+incompatible // indirect
-	github.com/openshift/operator-custom-metrics v0.4.3-0.20220322205053-7b528cc0d6eb // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring v0.55.0 // indirect
 	github.com/prometheus/client_model v0.4.0 // indirect
 	github.com/prometheus/common v0.42.0 // indirect
 	github.com/prometheus/procfs v0.9.0 // indirect
@@ -103,13 +105,9 @@ require (
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
-	github.com/sykesm/zap-logfmt v0.0.4 // indirect
 	github.com/xlab/treeprint v1.1.0 // indirect
 	go.starlark.net v0.0.0-20200306205701-8dd3e2ee1dd5 // indirect
-	go.uber.org/atomic v1.10.0 // indirect
-	go.uber.org/multierr v1.8.0 // indirect
-	go.uber.org/zap v1.24.0 // indirect
-	golang.org/x/mod v0.10.0 // indirect
+	golang.org/x/crypto v0.1.0 // indirect
 	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/oauth2 v0.5.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
@@ -122,17 +120,14 @@ require (
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
-	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/apiextensions-apiserver v0.27.2 // indirect
 	k8s.io/cli-runtime v0.27.4 // indirect
 	k8s.io/component-base v0.27.4 // indirect
-	k8s.io/gengo v0.0.0-20220902162205-c0856e24416d // indirect
 	k8s.io/klog/v2 v2.90.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20230501164219-8b0f38b5fd1f // indirect
 	k8s.io/utils v0.0.0-20230209194617-a36077c30491 // indirect
-	sigs.k8s.io/controller-tools v0.11.3 // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
 	sigs.k8s.io/kustomize/api v0.13.2 // indirect
 	sigs.k8s.io/kustomize/kyaml v0.14.1 // indirect
---
+++ pkg/handlers/cluster_test.go
@@ -0,0 +1,288 @@
+package handlers_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"time"
+
+	"github.com/gorilla/mux"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/ghttp"
+	sdk "github.com/openshift-online/ocm-sdk-go"
+	cmv1 "github.com/openshift-online/ocm-sdk-go/clustersmgmt/v1"
+	. "github.com/openshift-online/ocm-sdk-go/testing"
+	"github.com/openshift/ocm-agent/pkg/handlers"
+)
+
+var getCluster = `{
+	"kind": "Cluster",
+	"id": "string",
+	"href": "string",
+	"name": "string",
+	"external_id": "string",
+	"infra_id": "string",
+	"display_name": "",
+	"creation_timestamp": "2023-11-19T07:52:20.413713Z",
+	"activity_timestamp": "2023-11-20T10:17:18Z",
+	"expiration_timestamp": "2023-11-20T19:52:18.077225Z",
+	"cloud_provider": {
+	  "kind": "CloudProviderLink",
+	  "id": "aws",
+	  "href": "/api/clusters_mgmt/v1/cloud_providers/aws"
+	},
+	"openshift_version": "4.14.1",
+	"subscription": {
+	  "kind": "SubscriptionLink",
+	  "id": "subid",
+	  "href": "/api/accounts_mgmt/v1/subscriptions/subid"
+	},
+	"region": {
+	  "kind": "CloudRegionLink",
+	  "id": "us-west-2",
+	  "href": "/api/clusters_mgmt/v1/cloud_providers/aws/regions/us-west-2"
+	},
+	"console": {
+	  "url": "string"
+	},
+	"api": {
+	  "url": "string",
+	  "listening": "external"
+	},
+	"nodes": {
+	  "master": 3,
+	  "infra": 2,
+	  "compute": 2,
+	  "availability_zones": [
+		 "us-west-2a"
+	  ],
+	  "compute_machine_type": {
+		"kind": "MachineTypeLink",
+		"id": "m5.xlarge",
+		"href": "/api/clusters_mgmt/v1/machine_types/m5.xlarge"
+	  },
+	  "infra_machine_type": {
+		"kind": "MachineTypeLink",
+		"id": "r5.xlarge",
+		"href": "/api/clusters_mgmt/v1/machine_types/r5.xlarge"
+	  }
+	},
+	"state": "ready",
+	"flavour": {
+	  "kind": "FlavourLink",
+	  "id": "osd-4",
+	  "href": "/api/clusters_mgmt/v1/flavours/osd-4"
+	},
+	"groups": {
+	  "kind": "GroupListLink",
+	  "href": "/api/clusters_mgmt/v1/clusters/clusterid/groups"
+	},
+	"aws": {
+	  "private_link": false,
+	  "private_link_configuration": {
+		"kind": "PrivateLinkConfigurationLink",
+		"href": "/api/clusters_mgmt/v1/clusters/clusterid/aws/private_link_configuration"
+	  },
+	  "tags": {
+		"red-hat-clustertype": "osd",
+		"red-hat-managed": "true"
+	  },
+	  "audit_log": {
+		"role_arn": ""
+	  },
+	  "ec2_metadata_http_tokens": "optional"
+	},
+	"dns": {
+	  "base_domain": "string"
+	},
+	"network": {
+	  "type": "OVNKubernetes",
+	  "machine_cidr": "10.0.0.0/16",
+	  "service_cidr": "172.30.0.0/16",
+	  "pod_cidr": "10.128.0.0/16",
+	  "host_prefix": 23
+	},
+	"external_configuration": {
+	  "kind": "ExternalConfiguration",
+	  "href": "/api/clusters_mgmt/v1/clusters/clusterid/external_configuration",
+	  "syncsets": {
+		"kind": "SyncsetListLink",
+		"href": "/api/clusters_mgmt/v1/clusters/clusterid/external_configuration/syncsets"
+	  },
+	  "labels": {
+		"kind": "LabelListLink",
+		"href": "/api/clusters_mgmt/v1/clusters/clusterid/external_configuration/labels"
+	  },
+	  "manifests": {
+		"kind": "ManifestListLink",
+		"href": "/api/clusters_mgmt/v1/clusters/clusterid/external_configuration/manifests"
+	  }
+	},
+	"multi_az": false,
+	"managed": true,
+	"ccs": {
+	  "enabled": true,
+	  "disable_scp_checks": false
+	},
+	"version": {
+	  "kind": "Version",
+	  "id": "openshift-v4.14.1",
+	  "href": "/api/clusters_mgmt/v1/versions/openshift-v4.14.1",
+	  "raw_id": "4.14.1",
+	  "channel_group": "stable",
+	  "end_of_life_timestamp": "2025-02-28T00:00:00Z"
+	},
+	"identity_providers": {
+	  "kind": "IdentityProviderListLink",
+	  "href": "/api/clusters_mgmt/v1/clusters/clusterid/identity_providers"
+	},
+	"aws_infrastructure_access_role_grants": {
+	  "kind": "AWSInfrastructureAccessRoleGrantLink",
+	  "href": "/api/clusters_mgmt/v1/clusters/clusterid/aws_infrastructure_access_role_grants"
+	},
+	"addons": {
+	  "kind": "AddOnInstallationListLink",
+	  "href": "/api/clusters_mgmt/v1/clusters/clusterid/addons"
+	},
+	"ingresses": {
+	  "kind": "IngressListLink",
+	  "href": "/api/clusters_mgmt/v1/clusters/clusterid/ingresses"
+	},
+	"machine_pools": {
+	  "kind": "MachinePoolListLink",
+	  "href": "/api/clusters_mgmt/v1/clusters/clusterid/machine_pools"
+	},
+	"inflight_checks": {
+	  "kind": "InflightCheckListLink",
+	  "href": "/api/clusters_mgmt/v1/clusters/clusterid/inflight_checks"
+	},
+	"product": {
+	  "kind": "ProductLink",
+	  "id": "osd",
+	  "href": "/api/clusters_mgmt/v1/products/osd"
+	},
+	"status": {
+	  "state": "ready",
+	  "dns_ready": true,
+	  "oidc_ready": false,
+	  "provision_error_message": "",
+	  "provision_error_code": "",
+	  "configuration_mode": "full",
+	  "limited_support_reason_count": 0
+	},
+	"node_drain_grace_period": {
+	  "value": 60,
+	  "unit": "minutes"
+	},
+	"etcd_encryption": false,
+	"billing_model": "standard",
+	"disable_user_workload_monitoring": true,
+	"managed_service": {
+	  "enabled": false,
+	  "managed": false
+	},
+	"hypershift": {
+	  "enabled": false
+	},
+	"byo_oidc": {
+	  "enabled": false
+	},
+	"delete_protection": {
+	  "href": "/api/clusters_mgmt/v1/clusters/clusterid/delete_protection",
+	  "enabled": false
+	}
+  }`
+
+var clusterHandler *handlers.ClusterHandler
+var apiServer *ghttp.Server
+var responseRecorder *httptest.ResponseRecorder
+var internalId = "internal-id"
+
+var _ = Describe("ClusterHandler", func() {
+	BeforeEach(func() {
+		// Inspired by https://github.com/gdbranco/rosa/blob/67f55df2992b596e810942016833893236ef47f1/cmd/upgrade/cluster/cmd_test.go#L23
+		apiServer = MakeTCPServer()
+
+		accessToken := MakeTokenString("Bearer", 15*time.Minute)
+
+		sdkclient, _ := sdk.NewConnectionBuilder().
+			Logger(nil).
+			Tokens(accessToken).
+			URL(apiServer.URL()).
+			Build()
+
+		clusterHandler = handlers.NewClusterHandler(sdkclient, internalId)
+		responseRecorder = httptest.NewRecorder()
+
+	})
+
+	AfterEach(func() {
+		// Close the servers:
+		apiServer.Close()
+	})
+
+	It("should get a cluster object", func() {
+		makeOCMRequest(
+			"GET",
+			http.StatusOK,
+			fmt.Sprintf("/api/clusters_mgmt/v1/clusters/%s", internalId),
+			getCluster,
+		)
+		req := httptest.NewRequest("GET", "/cluster", nil)
+
+		req = mux.SetURLVars(
+			req,
+			map[string]string{},
+		)
+
+		clusterHandler.ServeClusterGet(responseRecorder, req)
+
+		var cluster cmv1.Cluster
+
+		// nolint
+		_ = json.NewDecoder(responseRecorder.Result().Body).Decode(&cluster)
+
+		var ocmResp cmv1.Cluster
+
+		// nolint
+		_ = json.Unmarshal([]byte(getCluster), &ocmResp)
+
+		Expect(reflect.DeepEqual(cluster, ocmResp)).To(BeTrue())
+	})
+
+	It("should return an error if ocm returns an error", func() {
+		errorMessage := `{"message": "Cannot connect to ocm api"}`
+		makeOCMRequest(
+			"GET",
+			http.StatusBadRequest,
+			fmt.Sprintf("/api/clusters_mgmt/v1/clusters/%s", internalId),
+			errorMessage,
+		)
+
+		req := httptest.NewRequest("GET", "/cluster", nil)
+
+		clusterHandler.ServeClusterGet(responseRecorder, req)
+
+		Expect(responseRecorder.Result().StatusCode).To(Equal(http.StatusBadRequest))
+	})
+
+})
+
+func makeOCMRequest(method string, status int, route string, ocmResponse string) {
+	handler := RespondWithJSON(status, ocmResponse)
+	if status == http.StatusNoContent {
+		handler = ghttp.RespondWith(
+			status,
+			nil,
+			http.Header{
+				"Content-Type": []string{
+					"application/json",
+				},
+			},
+		)
+	}
+	apiServer.RouteToHandler(method, route, handler)
+}
